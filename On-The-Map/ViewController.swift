cc9291b1bd7deef51d63c13c4b91d5378fda4474
Curtes Malteser <antonio_bastiao@hotmail.com>
2020-07-22 22:24:40 +0200

    project rebuild one more time

---
+++ On-The-Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On-The-Map
+//
+//  Created by António Bastião on 22.07.20.
+//  Copyright © 2020 António Bastião. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
