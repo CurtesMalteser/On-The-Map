a78cae4b862110348515e8ca3accf3f25767aee5
Curtes Malteser <antonio_bastiao@hotmail.com>
2020-07-26 22:46:43 +0200

    populates table with students info

--- On-The-Map/Controller/StudentsLocationTableVC.swift
+++ On-The-Map/Controller/StudentsLocationTableVC.swift
@@ -10,31 +10,57 @@ import UIKit
 
 class StudentsLocationTableVC:UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    let studentList = [
-        "lol",
-        "ahah"
-    ]
+    private var studentLocationList: [StudentLocation] = []
     
-    override func viewWillAppear(_ animated: Bool) {
-           
-       }
+    @IBOutlet var studentLocationTable: UITableView!
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        guard let studentsURL = URL(string: UdacityAPI.getListOfStudentLocation.rawValue) else {
+            print("Cannot create URL")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: studentsURL) {
+            (data, response, error) in
+            guard let data = data else {
+                print("no data")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let studentLocationList = try decoder.decode(StudentList.self, from: data)
+                DispatchQueue.main.async {
+                    self.studentLocationList = studentLocationList.results
+                    
+                    self.studentLocationTable.reloadData()
+                }
+            } catch {
+                print(error)
+            }
+            
+        }
+        
+        task.resume()
         
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        
-        return studentList.count
+        return studentLocationList.count
        }
        
        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
            
         //StudentLocationCell
         let cell = tableView.dequeueReusableCell(withIdentifier: "StudentLocationCell")
-        cell?.textLabel?.text = studentList[indexPath.row]
-        cell?.detailTextLabel?.text = studentList[indexPath.row]
+        cell?.textLabel?.text = studentLocationList[indexPath.row].firstName // todo: Add last name
+        cell?.detailTextLabel?.text = studentLocationList[indexPath.row].mediaURL
       
         return cell!
        
