db696cc3e3a80cb3a3432d3929e8b1f7a8f4fd36
Curtes Malteser <antonio_bastiao@hotmail.com>
2020-08-30 14:45:53 +0200

    added LoginVC.swift

---
+++ On-The-Map/Controller/LoginVC.swift
@@ -0,0 +1,13 @@
+//
+//  LoginVC.swift
+//  On-The-Map
+//
+//  Created by António Bastião on 30.08.20.
+//  Copyright © 2020 António Bastião. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC:UIViewController {
+    
+}
