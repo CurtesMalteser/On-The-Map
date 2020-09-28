688b82744bc004e2073d8fe5b516bf0aa15ec66a
Antonio <antonio.bastiao@igenius.ai>
2020-09-28 22:27:19 +0200

    added alert if login API response is error

--- On-The-Map/Controller/LoginVC.swift
+++ On-The-Map/Controller/LoginVC.swift
@@ -34,26 +34,42 @@ class LoginVC:UIViewController {
         let request = UdacityAPI.initLoginRequest(url: loginURL, username: email, password: password)
         
         UdacityAPI.executeDataTask(request: request,
-                        sucessHandler: { data in self.onLoginSucess(data: data) },
-                        errorHandler: { error in print("error \(String(describing: error))")})
+                                   sucessHandler: { data in self.onLoginSucess(data: data) },
+                                   errorHandler: { error in
+                                    
+                                    guard let udacityError = error as? UdacityError else {
+                                        self.showErrorAlert(message:  error?.localizedDescription ?? "Undefined Error")
+                                        return
+                                    }
+                                    
+                                    if(udacityError.status == 403) {
+                                        self.showErrorAlert(message:  udacityError.error)
+                                    } else {
+                                        print("error \(udacityError.error) \(udacityError.status)")
+                                        // todo -> check if there are other errors, but 400 is empty credentials and
+                                        // is better avoind unecessary api call, byt checking if credentials are valid email
+                                        // or pass isn't empty. Extract to funcrion
+                                        self.showErrorAlert(message:  udacityError.error)
+                                    }
+                                    
+                                    
+                                   })
+    }
+    
+    private func showErrorAlert(message: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Error", message: message, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
     
     
     
     private func onLoginSucess(data: Data) {
-        
-        
-        if let data = String(data: data, encoding: .utf8) {
-        
-            print("this is data \(data)")
-        }
-        
-        
-        
         DispatchQueue.main.async {
             self.segueOnLoginSuccess()
         }
-        
     }
     
     // Performs segue to Tab Controller
