3b068837b062e6f51a66805a0823879ad9a5e4dd
Antonio <antonio.bastiao@igenius.ai>
2020-10-05 23:17:21 +0200

    got geocoding from address typed in the addAddressTextField

--- On-The-Map/Controller/AddLocationVC.swift
+++ On-The-Map/Controller/AddLocationVC.swift
@@ -7,7 +7,33 @@
 //
 
 import UIKit
+import CoreLocation
 
 class AddLocationVC : UIViewController {
+    
+    @IBOutlet weak var addAddressTextField: UITextField!
+    
+    @IBOutlet weak var addUrlTextField: UITextField!
+    
+    @IBAction func getGeoCoding(_ sender: Any) {
+        
+        guard let address = addAddressTextField.text else {
+            return
+        }
+        
+        CLGeocoder().geocodeAddressString(address) {placemarks, error in
+            
+            if let error = error {
+                print("Error", error)
+                return
+            }
+            
+            if let location = placemarks?.first?.location {
+                let coordinates:CLLocationCoordinate2D = location.coordinate
+                print(coordinates)
+            }
+            
+        }
+    }
     
 }
